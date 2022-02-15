5e77a549d98d6df8c99874fbbb494dabfe421e29
bhupendragtm <bhupendragtm777@gmail.com>
2022-02-15 15:41:24 +0545

    Task Status Problem Solved

---
+++ src/tasks/task-status.enum.ts
@@ -0,0 +1,6 @@
+
+export enum TasksStatus {
+    OPEN = 'OPEN',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE',
+  }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid/v1';
+import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
@@ -38,7 +38,7 @@ export class TasksService {
     const { title, description } = createTaskDto;
 
     const task: Task = {
-      id: uuid(),
+      id: uuidv4(),
       title,
       description,
       status: TaskStatus.OPEN,
