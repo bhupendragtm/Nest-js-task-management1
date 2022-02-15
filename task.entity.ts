5253557e505361b29b96ee1f9d2ef81b58a09f36
bhupendragtm <bhupendragtm777@gmail.com>
2022-02-15 15:29:47 +0545

    All files Are Added

---
+++ task.entity.ts
@@ -0,0 +1,18 @@
+import { TasksStatus } from 'src/tasks/task-status.enum';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn('uuid')
+     id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: TasksStatus;
+}
+
