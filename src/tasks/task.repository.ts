5e76fa4f7857302137bbab59dbf5ee1aacdaac4e
bhupendragtm <bhupendragtm777@gmail.com>
2022-02-20 22:43:13 +0545

    Task Entity has been added

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -24,12 +24,7 @@ export class TaskRepository extends Repository<Task> {
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
-
-    // const task = new Task();
-    // task.title = title;
-    // task.description = description;
-    // task.status = TaskStatus.OPEN;
-    // await task.save();
+    
     const task = await this.create({title, description});
     await this.save(task);
     return task;
